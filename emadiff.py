897dcb9d04f79efb54fc75ac383c9fa079f9b84b
prashlkam <75939694+prashlkam@users.noreply.github.com>
2021-03-08 09:31:28 +0530

    emadiff - errors in code corrected...........
    emadiff - errors in code corrected...........

--- emadiff.py
+++ emadiff.py
@@ -6,10 +6,13 @@ class emadiff:
 
     def wrapper_Least_ema_diff(stocks, twemas, hundemas):
         Stocks_short_list = []
-        # least_emadiff = 100
         threshold = 0.25
-        for stock, tema, hundema in range(stocks, twemas, hundemas):
-            tempdiff = twe_hund_ema_diff(twema, hundema)
-            if tempdiff <= threshold:
-                Stocks_short_list.append(stock)
+        stock_list_size = len(stocks)
+        if stock_list_size == len(twemas) and stock_list_size == len(hundemas):
+            for i in range(stock_list_size):
+                tempdiff = twe_hund_ema_diff(twemas[i], hundemas[i])
+                if tempdiff <= threshold:
+                    Stocks_short_list.append(stocks[i])
+        else:
+            print('List length mismatch - ensure that all 3 lists are of the same size...')
         return Stocks_short_list
